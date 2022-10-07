54a7637643e894a160d69c9c482592477d98b0b6
kabo_g <kabogalefete@gmail.com>
2022-10-07 09:34:26 +0800

    add model to the admin console

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . models import Signature
 # Register your models here.
+
+class SignatureAdmin(admin.ModelAdmin):
+
+    list_display = ["firstname", "lastname", "email"]
+
+
+admin.site.register(Signature, SignatureAdmin)
